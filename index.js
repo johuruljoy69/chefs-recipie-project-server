0994dd89c6c517fcd208433111d338508aed954a
johirjoy <joy.sd69@gmail.com>
2023-05-02 15:28:00 +0600

    data api loading

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-// const cors = require('cors')
 const port = process.env.PORT || 5000
 
-// app.use(cors());
+app.use(cors());
 
 app.get('/', (req, res) =>{
     res.send('JS FoodHub is running')
 })
 
 app.listen(port, () =>{
-    console.log(`Dragon API is running on port : ${port}`);
+    console.log(`JS FoodHub API is running on port : ${port}`);
 })
 
+const chefs = require('./data/chefData.json')
+
+app.get('/chefs', (req, res) =>{
+    res.send(chefs);
+})
+
+app.get('/chefs/:id', (req, res) =>{
+    const id = req.params.id;
+    console.log(id);
+    const selectedChef = chefs.find(chef => chef.id === id)
+    res.send(selectedChef);
+})
+
+app.get('/all-chefs/:id', (req, res) =>{
+    const id = parseInt(req.params.id)
+    if(id === 0){
+        res.send(chefs)
+    }
+    else{
+        const allChefs = chefs.filter(chef => parseInt(chef.chef_id) === id)
+        res.send(allChefs); 
+    }
+})
