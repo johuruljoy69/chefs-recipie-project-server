a27f2ad6cce9f846cdebde85f48f10032bc5a9d9
johirjoy <joy.sd69@gmail.com>
2023-05-02 11:14:37 +0600

    server running

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+// const cors = require('cors')
+const port = process.env.PORT || 5000
+
+// app.use(cors());
+
+app.get('/', (req, res) =>{
+    res.send('JS FoodHub is running')
+})
+
+app.listen(port, () =>{
+    console.log(`Dragon API is running on port : ${port}`);
+})
+
